f3335e0188f6132a46a7feb034254dac275156fc
zhangchao <zc9286@live.cn>
2020-11-24 19:58:53 +0800

    fix: free memory of string arrs

--- c_useful_demo/str_arr_malloc/test.c
+++ c_useful_demo/str_arr_malloc/test.c
@@ -1,33 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-char ** gpio_grp;
+char ** chat_grp;
 int main(int argc,char **argv){
 	int i;
-	int grp_num;
+	int string_num;
 	int str_len;
 	
-	grp_num = argc;
-	printf("grp number is %d\n",grp_num);
-	gpio_grp = (char **)malloc(sizeof(char *)*grp_num);
-//	printf("argv: %s\n",argv[1]);
-	for (i = 0; i < grp_num; i++){
-		
+	string_num = argc;
+	printf("grp number is %d\n",string_num);
+	chat_grp = (char **)malloc(sizeof(char *)*string_num);
+	for (i = 0; i < string_num; i++){
 		if(argv[i] == NULL) break;
 		str_len = strlen(argv[i]);
 		printf("str_len:%d\n",str_len);		
-		gpio_grp[i] = (char *)malloc(sizeof(char) * str_len);
-		if(gpio_grp[i] == NULL) printf("malloc err\n");	
+		chat_grp[i] = (char *)malloc(sizeof(char) * str_len);
+		if(chat_grp[i] == NULL) printf("malloc err\n");	
 		else {
-			printf("dada\n");
-			strcpy(gpio_grp[i],argv[i]);
-			printf("gpio_grp %d is %s\n",i,gpio_grp[i]);
+			printf("chat_grp[%d]: addr : %p allocated!\n",i,chat_grp[i]);
+			strcpy(chat_grp[i],argv[i]);
+			printf("chat_grp %d is %s\n",i,chat_grp[i]);
 		}
 	}
-	printf("gpio_grp addr : %p  is :%s\n",gpio_grp,gpio_grp[0]);
-	printf("gpio_grp + 1 addr : %p size is :%s\n",gpio_grp+1,gpio_grp[1]);
-	free(gpio_grp);
-	gpio_grp = NULL;
-	printf("gpio_grp addr %p is %s\n",gpio_grp[1],*(gpio_grp+1));
-
+	/*release the memory allocated*/
+	for(i = string_num-1; i >= 0; i--){
+		printf("chat_grp[%d]: addr : %p freed!\n",i,chat_grp[i]);
+		free(chat_grp[i]);
+		chat_grp[i] = NULL;
+	}
+	free(chat_grp);
+	chat_grp = NULL;
 }
