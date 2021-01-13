4c9458229206a90f9d03c4e12dce1685d76dfffc
zhangchao <zc9286@live.cn>
2021-01-13 19:20:22 +0800

    Add: leet_code no.3 accepted via hash array

--- leet_code/3/test.c
+++ leet_code/3/test.c
@@ -6,7 +6,8 @@
 
 // @lc code=start
 #include <stdio.h>
-
+#include <string.h>
+#if 0
 int lengthOfLongestSubstring(char * s){
     char *string_s = s;
     int num = 0;;
@@ -36,12 +37,28 @@ int lengthOfLongestSubstring(char * s){
     return num;
 
 }
-
+#endif
+int lengthOfLongestSubstring(char * s){
+	char char_sets[128] = {0};
+	int left = 0;
+	int right = 0;
+	int max_length = 0;
+	int string_length = strlen(s);
+	
+	while(left < string_length && right < string_length){	
+		if(char_sets[s[right]] == 0){
+			char_sets[s[right]] = 1;
+			right++;
+			max_length = max_length > right-left ? max_length : right-left;	
+		}else{
+			char_sets[s[left]] = 0;
+			left++;			
+		}
+	}
+	return max_length;
+}
 int main(int argc ,int *argv[]){
-	
-	
 	char *s = "dvdf"; 
-	
 	printf("result: %d\n",lengthOfLongestSubstring(s));
 }
 // @lc code=end
