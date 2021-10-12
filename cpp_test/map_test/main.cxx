8254c5446eb536afdcc801bd73cb574ff20619fe
zhangchao <zc9286@live.cn>
2021-10-12 11:03:24 +0800

    Important: the proper way of using map.erase()

--- cpp_test/map_test/main.cxx
+++ cpp_test/map_test/main.cxx
@@ -1,20 +1,27 @@
-#include <iostream> 
-#include <map> 
-using namespace std; 
-  
-int main() 
-{ 
-    // declaration of map container 
-    map<char, int> mymap; 
-    mymap['a'] = 1; 
-    mymap['b'] = 2; 
-    mymap['c'] = 3; 
-  
-    // using begin() to print map 
-	std::map<char, int>::iterator it;
+#include <iostream>
+#include <map>
+using namespace std;
 
-    for (it = mymap.begin(); it != mymap.end(); ++it) 
-        cout << it->first << " = "
-             << it->second << '\n'; 
-    return 0; 
+int main()
+{
+    // declaration of map container
+    map<char, int> mymap;
+    mymap['a'] = 1;
+    mymap['b'] = 2;
+#if 0
+    mymap['c'] = 3;
+#endif
+    std::map<char, int>::iterator it;
+
+    std::cout << "map size: "<<mymap.size() << std::endl;
+    for (it = mymap.begin(); it != mymap.end();it++){
+        cout << it->first << " = "<< it->second << '\n';
+        mymap.erase(it++);
+//or use like:
+//		it = mymap.erase(it);
+		cout << "map size update: "<<mymap.size() << std::endl;
+    }
+
+    mymap.clear();
+    return 0;
 }
