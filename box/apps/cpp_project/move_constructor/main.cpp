c1e119cb2ba2ad6ce78ed1c865b3c265de0ff68d
zhangchao <zc9286@live.cn>
2021-11-26 15:48:13 +0800

    Add: moce constructor right referrence

---
+++ box/apps/cpp_project/move_constructor/main.cpp
@@ -0,0 +1,101 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+class MyString
+{
+public:
+    static size_t DCtor; //统计调用默认构造函数的次数
+    static size_t CCtor; //统计调用拷贝构造函数的次数
+    static size_t MCtor; //统计调用移动构造函数的次数
+    static size_t CAsgn; //统计调用拷贝赋值函数的次数
+    static size_t MAsgn; //统计调用移动赋值函数的次数
+
+public:
+    // 构造函数
+   MyString(const char* cstr=0){
+
+       DCtor++;
+
+       if (cstr) {
+          m_data = new char[strlen(cstr)+1];
+          strcpy(m_data, cstr);
+       }
+       else {
+          m_data = new char[1];
+          *m_data = '\0';
+       }
+   }
+
+   // 拷贝构造函数
+   MyString(const MyString& str) {
+       CCtor ++;
+       m_data = new char[ strlen(str.m_data) + 1 ];
+       strcpy(m_data, str.m_data);
+   }
+#if 1 
+   // 移动构造函数
+   MyString(MyString&& str) noexcept
+       :m_data(str.m_data) {
+       MCtor ++;
+       str.m_data = nullptr; //不再指向之前的资源了
+   }
+#endif
+   // 拷贝赋值函数 =号重载
+   MyString& operator=(const MyString& str){
+       CAsgn ++;
+       if (this == &str) // 避免自我赋值!!
+          return *this;
+
+       delete[] m_data;
+       m_data = new char[ strlen(str.m_data) + 1 ];
+       strcpy(m_data, str.m_data);
+       return *this;
+   }
+
+   // 移动赋值函数 =号重载
+   MyString& operator=(MyString&& str) noexcept{
+       MAsgn ++;
+       if (this == &str) // 避免自我赋值!!
+          return *this;
+
+       delete[] m_data;
+       m_data = str.m_data;
+       str.m_data = nullptr; //不再指向之前的资源了
+       return *this;
+   }
+
+   ~MyString() {
+       delete[] m_data;
+   }
+
+   char* get_c_str() const { return m_data; }
+private:
+   char* m_data;
+};
+size_t MyString::DCtor = 0;
+size_t MyString::CCtor = 0;
+size_t MyString::MCtor = 0;
+size_t MyString::CAsgn = 0;
+size_t MyString::MAsgn = 0;
+int main()
+{
+    vector<MyString> vecStr;
+    vecStr.reserve(1000); //先分配好1000个空间
+    for(int i=0;i<100000;i++){
+        vecStr.push_back(MyString("hello"));  //MyString("hello") 是临时对象，是个右值。优先调用移动构造函数
+    }
+    cout << "默认构造函数被调用次数：DCtor = " << MyString::DCtor << endl;
+    cout << "拷贝构造函数被调用次数：CCtor = " << MyString::CCtor << endl;
+    cout << "移动构造函数被调用次数：MCtor = " << MyString::MCtor << endl;
+    cout << "拷贝赋值运算符：CAsgn = " << MyString::CAsgn << endl;
+    cout << "移动赋值运算符：MAsgn = " << MyString::MAsgn << endl;
+}
+
+/* 结果
+CCtor = 0
+MCtor = 1000
+CAsgn = 0
+MAsgn = 0
+*/
